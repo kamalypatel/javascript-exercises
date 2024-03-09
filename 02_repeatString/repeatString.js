6b6e88eeec01ab1dc1de557421a87738bbb47ad5
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-03-09 12:04:51 -0500

    added if statement to consider 0 iterations

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,17 @@
 const repeatString = function(textInput, numberOfTextInputs) {
-    let textOutput = textInput
-    for (let i = 1; i < numberOfTextInputs; i++) {
-        textOutput = textOutput + textInput
-      }
+    if (numberOfTextInputs == 0) {
+        let textOutput = ''
+        
+        return textOutput;
+    } else if (numberOfTextInputs > 0) {
+        let textOutput = textInput
 
-      return textOutput
+        for (let i = 1; i < numberOfTextInputs; i++) {
+            textOutput = textOutput + textInput
+        }
+
+        return textOutput;
+    }
 };
 
 // Do not edit below this line
