fe0e298df16c02e28950685fd4d61e83d34bd27a
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-03-09 16:07:38 -0500

    added initial logic to test for if a year is a leap year per the definition of a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(yearInput) {
+    if (yearInput % 400 == 0) {
+        return true
+    } else if (yearInput % 4 == 0) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
