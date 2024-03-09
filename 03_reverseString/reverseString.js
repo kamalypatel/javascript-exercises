d1f28d7e83a8a6ffbb91afee7301684643321c94
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-03-09 14:59:24 -0500

    made logic to pass the first test of exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(inputText) {
 
+    let reverse = ''
+
+    for (let i = inputText.length - 1; i >= 0; i--) {
+        console.log(i)
+        reverse = reverse + inputText.charAt(i)
+        console.log(reverse)
+    }
+    return reverse
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+console.log(reverseString('hello'))
