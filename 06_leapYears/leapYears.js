8d33472e6994405772ead7c22ef76dec16f00e9c
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-03-09 16:12:20 -0500

    added logic for century years. had to adjust for the 400 year rule

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,6 @@
 const leapYears = function(yearInput) {
-    if (yearInput % 400 == 0) {
-        return true
+    if (yearInput % 100 == 0 && yearInput % 4 == 0 && yearInput % 400 != 0) {
+        return false
     } else if (yearInput % 4 == 0) {
         return true
     } else {
