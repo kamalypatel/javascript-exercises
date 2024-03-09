dff55f4101ae1fa0ffcd93891e7692b6cf6a22d6
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-03-09 16:25:22 -0500

    fixed type in C to F conversion and added logic for rounding. Also passed all tests for exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(inputTemp) {
-  return (inputTemp - 32) * 5/9
+  return Math.round((inputTemp - 32) * 5/9 * 10) /10 
 };
 
 const convertToFahrenheit = function(inputTemp) {
-  return inputTemp * 5 / 9 + 32
+  return Math.round(((inputTemp *  9 / 5) + 32) * 10) / 10
 };
 
 // Do not edit below this line
