09b97dff1bcab18069adf87f33ca084290e3cf21
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-03-16 09:45:40 -0400

    added logic for palindrome builder and check

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (palindromeString) {
+    let newString = ''
+    for (let i = palindromeString.length - 1; i >= 0; i--) {
+        newString += palindromeString.charAt(i)
+    }
+    
+    return newString == palindromeString ? true : false
+    
 };
 
 // Do not edit below this line
