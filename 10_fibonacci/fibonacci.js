de3f86c37dc6a69204b973d195d0837ffebe751e
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-03-16 10:34:22 -0400

    made a fibonacci adding function starting at 0,1

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(sequenceNumber) {
+    let arr = [0, 1]
+    let sum = 1
+    for (let i = 1; i <= sequenceNumber; i++) {
+        sum = arr[i] + arr[i-1];
+        arr.push(sum)
+    }
+    return arr[arr.length-2]
 };
 
 // Do not edit below this line
