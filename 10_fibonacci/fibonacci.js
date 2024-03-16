f6e6d3edaef84f815b72ba413adef4d505b8870e
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-03-16 11:14:03 -0400

    added check for negative and strings

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,11 +1,16 @@
 const fibonacci = function(sequenceNumber) {
     let arr = [0, 1]
     let sum = 1
-    for (let i = 1; i <= sequenceNumber; i++) {
-        sum = arr[i] + arr[i-1];
-        arr.push(sum)
+    sequenceNumber = parseInt(sequenceNumber)
+    if (sequenceNumber < 0) {
+            return 'OOPS'
+        } else {
+        for (let i = 1; i <= sequenceNumber; i++) {
+            sum = arr[i] + arr[i-1];
+            arr.push(sum)
+        }
+        return arr[arr.length-2]
     }
-    return arr[arr.length-2]
 };
 
 // Do not edit below this line
